77a05c3590fde462584df55a7251131978b61982
cw0925 <416560407@qq.com>
2020-04-24 13:02:17 +0800

    网络请求成功

---
+++ yqoa/lib/app/net/API.dart
@@ -0,0 +1,4 @@
+class API {
+  static final host = 'http://ivy.dp.youqii.com';
+  static final loginUrl = host + '/api/authorizations';
+}
--- yqoa/lib/app/net/ApiInterface.dart
+++ yqoa/lib/app/net/ApiInterface.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
-import 'package:dio/dio.dart';
-//import 'package:http/http.dart';
 import 'NetUtil.dart';
 import 'common_error_handler_utils.dart';
+import 'API.dart';
 
 /// 所有接口请求
 
@@ -22,5 +19,11 @@ class ApiInterface {
       LoginInvalidHandler handler) async {
     return NetUtil.getJson(_API_GET_TEAM_FUND, {})
         .catchError(handler.loginInvalidHandler);
+  }
+
+  static Future<Map<String, dynamic>> loginRequest(
+      String username, String password) async {
+    return NetUtil.postJson(API.loginUrl,
+        {"username": username, "password": password});
   }
 }
