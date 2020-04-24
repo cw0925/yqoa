bd7278d0177805fd59337861b6c20d5b03e96721
cw0925 <416560407@qq.com>
2020-04-24 14:11:01 +0800

    本地存储

--- yqoa/lib/app/home/CRM.dart
+++ yqoa/lib/app/home/CRM.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yqoa/app/widget/CustomAppbar.dart';
+import 'package:yqoa/app/utils/SharedPreferencesUtil.dart';
 
 class CRM extends StatelessWidget {
   // This widget is the root of your application.
@@ -41,5 +42,17 @@ class _CRMPageState extends State<CRMPage> {
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SharedPreferencesUtil spDataUtil = new SharedPreferencesUtil();
+
+    var name = spDataUtil.getStringInfo("name").toString();
+    var sex = spDataUtil.getStringInfo("sex");
+
+    print(name);
+    print(sex);
   }
 }
